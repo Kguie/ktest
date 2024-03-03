9ac32c21e76b775fe3c49d232876c3a1c574c5ff
Kévin Guieba <107431452+Kguie@users.noreply.github.com>
2024-03-02 22:14:48 -0400

    Mise en place d'unte taille standard pour les cartes

--- app/screens/animatedFlatlist.tsx
+++ app/screens/animatedFlatlist.tsx
@@ -1,16 +1,88 @@
-import { FlatList, ScrollView } from "react-native-gesture-handler";
+// import { FlatList, ScrollView } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, ScrollView, FlatList, Animated } from "react-native";
+
 import { useGetPhotos } from "../../hooks/api/photos";
-import { useEffect } from "react";
+import { margins } from "../../config/theme";
+import PostCard from "../../components/posts/postCard";
+import { PhotoProps } from "../../utils/photos";
+import { useCallback, useMemo } from "react";
+
+const COUNT_TO_RENDER: number = 1;
 
 export default function AnimatedFlatList() {
-  const { data } = useGetPhotos();
+  const { data, isLoading, isRefetching, refetch } = useGetPhotos();
+
+  const y = new Animated.Value(0);
+  const onScroll = Animated.event([{ nativeEvent: { contentOffset: { y } } }], {
+    useNativeDriver: true,
+  });
+  const AnimatedFlatList = Animated.createAnimatedComponent(FlatList<any>);
+
+  const postsCardSkeletonData: PhotoProps[] = useMemo(() => {
+    let resultArray: PhotoProps[] = [];
+    for (let i = 1; i <= 5; i++) {
+      resultArray.push({
+        url: "",
+        download_url: "",
+        id: `skeleton${i}`,
+        author: "",
+        height: 1,
+        width: 1,
+      });
+    }
+    return resultArray;
+  }, []);
+
+  const dataToDisplay: PhotoProps[] = useMemo(() => {
+    return isLoading ? postsCardSkeletonData : data;
+  }, [isLoading]);
 
   return (
-    <ScrollView>
-      <SafeAreaView edges={["bottom"]}>
-        <FlatList data={data} renderItem={undefined} />
-      </SafeAreaView>
-    </ScrollView>
+    <SafeAreaView style={styles.container} edges={["bottom"]}>
+      <AnimatedFlatList
+        scrollEventThrottle={16}
+        contentContainerStyle={styles.listContainer}
+        bounces={false}
+        // maxToRenderPerBatch={COUNT_TO_RENDER}
+        // initialNumToRender={COUNT_TO_RENDER}
+        // onEndReachedThreshold={0.03}
+        style={{ flex: 1 }}
+        data={dataToDisplay}
+        onRefresh={() => refetch()}
+        refreshing={isRefetching}
+        renderItem={({
+          item,
+          index,
+        }: {
+          item: PhotoProps;
+          index: number;
+        }): any => (
+          <PostCard
+            key={"post-" + item.id}
+            isLoading={isLoading}
+            post={item}
+            y={y}
+            index={index}
+          />
+        )}
+        {...{ onScroll }}
+      />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: margins.small,
+    paddingHorizontal: margins.xSmall,
+    flex: 1,
+    backgroundColor: "grey",
+  },
+  listContainer: {
+    display: "flex",
+    flexDirection: "column",
+    gap: margins.medium,
+    paddingBottom: margins.medium,
+  },
+});
--- components/posts/postCard.tsx
+++ components/posts/postCard.tsx
@@ -1,7 +1,8 @@
-import { Card } from "@rneui/themed";
-import { View } from "react-native";
+import { Card, Image, Skeleton } from "@rneui/themed";
+import { Animated, View } from "react-native";
 import { Dimensions, StyleSheet } from "react-native";
 import { Text } from "@rneui/themed";
+import { useMemo, useState } from "react";
 
 import { PhotoProps } from "../../utils/photos";
 import { DEFAULT_ROUNDNESS, margins } from "../../config/theme";
@@ -9,22 +10,96 @@ import { getViewHeight } from "../../utils";
 
 type Props = {
   post: PhotoProps;
+  isLoading: boolean;
+  y: Animated.Value;
+  index: number;
 };
 
-export default function PostCard({ post }: Props) {
+const DEFAULT_CARD_HEIGHT = getViewHeight(35);
+
+const DEFAULT_TEXT =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur, tellus vel pulvinar vestibulum, elit erat posuere nisl, ac tincidunt libero nunc eget orci. In metus libero, mattis sed leo ut, tristique dictum est. Nam blandit pellentesque condimentum. Pellentesque nec metus quis nisi eleifend maximus eget sit amet lectus. Etiam id pellentesque metus, sit amet mattis nunc. Fusce condimentum semper venenatis. Ut placerat euismod mauris, sit amet porttitor tellus vestibulum pretium leo.";
+
+export default function PostCard({ post, isLoading, y, index }: Props) {
+  const [cardDimensions, setCardDimensions] = useState({ width: 0, height: 0 });
+
+  const { height: screenHeight } = Dimensions.get("window");
+  // const height = screenHeight - 64;
+
+  const onLayout = (event: {
+    nativeEvent: { layout: { width: any; height: any } };
+  }) => {
+    const { width, height } = event.nativeEvent.layout;
+    setCardDimensions({ width, height: height + margins.xSmall });
+  };
+
+  // const position = Animated.subtract(index * cardDimensions.height, y);
+  // const isDisappearing = -cardDimensions.height;
+  // const isTop = 0;
+  // const isBottom = height - cardDimensions.height;
+  // const isAppearing = height;
+  const translateY = Animated.add(
+    y,
+    y.interpolate({
+      inputRange: [0, index * (DEFAULT_CARD_HEIGHT + margins.medium)],
+      outputRange: [0, -index * (DEFAULT_CARD_HEIGHT + margins.medium)],
+      extrapolateRight: "clamp",
+    })
+  );
+  // const scale = position.interpolate({
+  //   inputRange: [isDisappearing, isTop, isBottom, isAppearing],
+  //   outputRange: [0.5, 1, 1, 0.5],
+  //   extrapolate: "clamp",
+  // });
+  // const opacity = position.interpolate({
+  //   inputRange: [isDisappearing, isTop, isBottom, isAppearing],
+  //   outputRange: [0.5, 1, 1, 0.5],
+  // });
+
+  const text: string = useMemo(() => {
+    return DEFAULT_TEXT.split(" ")
+      .slice(Math.random() * 100)
+      .toString();
+  }, []);
+
   return (
-    <Card>
-      <View>
-        <Card.Image
-          resizeMode="contain"
-          style={{ aspectRatio: post.width / post.height, flex: 1 }}
-          src={post.url}
-        />
+    <Animated.View
+      // onLayout={onLayout}
+      style={{ ...styles.card, transform: [{ translateY }] }}
+      key={index}>
+      <View style={styles.imageCard}>
+        {isLoading ? (
+          <Skeleton height={getViewHeight(20)} />
+        ) : (
+          <Image
+            resizeMethod="scale"
+            resizeMode="cover"
+            height={getViewHeight(20)}
+            style={{ aspectRatio: 1 }}
+            alt={"post-" + post.id}
+            source={{ uri: post.download_url }}
+          />
+        )}
       </View>
-      <View>
-        <Text>{post.author}</Text>
+      <View style={styles.bodyCard}>
+        {isLoading ? (
+          <Skeleton height={16} width={"50%"} />
+        ) : (
+          <Text style={{ fontWeight: "bold", fontSize: 16 }}>
+            {post.author}
+          </Text>
+        )}
+        {isLoading ? (
+          <Skeleton
+            height={16}
+            width={"100%"}
+            style={{ height: getViewHeight(10) }}
+          />
+        ) : (
+          <Text style={{ fontSize: 14 }}>{text}</Text>
+        )}
       </View>
-    </Card>
+    </Animated.View>
   );
 }
 
@@ -32,7 +107,20 @@ const styles = StyleSheet.create({
   card: {
     borderRadius: DEFAULT_ROUNDNESS + margins.xSmall,
     flexDirection: "column",
-    minHeight: getViewHeight(10),
+    height: DEFAULT_CARD_HEIGHT,
     overflow: "hidden",
+    backgroundColor: "white",
+  },
+  imageCard: {
+    height: getViewHeight(20),
+    backgroundColor: "black",
+    overflow: "hidden",
+  },
+  bodyCard: {
+    flexDirection: "column",
+    backgroundColor: "white",
+    padding: margins.small,
+    gap: margins.small,
+    height: getViewHeight(10),
   },
 });
